9cd77a8c8c877c76d5021be90ef50f4af315b6d4
vladvaskiv <vlad.vaskiv@gmail.com>
2017-11-24 13:22:52 +0200

    Finish static pages with contacts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
         it "should have the content 'Sample App'" do
         visit '/static_pages/home'
@@ -8,7 +9,7 @@ describe "StaticPages" do
         end
         it "should have the right title 'Home'" do
           visit '/static_pages/home'
-          expect(page).to have_title("Home")
+          expect(page).to have_title("#{base_title} | Home")
         end
   end
   describe "Help page" do
@@ -18,7 +19,7 @@ describe "StaticPages" do
     end
     it "should have the right title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   describe "About page" do
@@ -28,7 +29,18 @@ describe "StaticPages" do
     end
     it "should have the right title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have the right title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 
